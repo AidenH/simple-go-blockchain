5c59483ce4f814b600bb477ebb05fe230c23dd76
AidenH <aidenalexh@gmail.com>
2021-10-02 19:45:00 -0400

    Create testing outlines

---
+++ main_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewChain(t *testing.T) {
+	fmt.Println()
+}
+
+func TestNewBlock(t *testing.T) {
+	fmt.Println()
+}
+
+func TestFindHash(t *testing.T) {
+	fmt.Println()
+}
