011814b50b1b68bc5895739db7dd7d87e18a6553
AidenH <aidenalexh@gmail.com>
2021-10-05 01:52:44 -0400

    Chain print formatting

--- main.go
+++ main.go
@@ -19,6 +19,11 @@ func main() {
 	fmt.Printf("BLOCKS:\n")
 
 	for b := range b1.ChainSlice {
-		fmt.Printf("%+v\n\n", b1.ChainSlice[b])
+		//fmt.Printf("%+v\n\n", b1.ChainSlice[b])
+		fmt.Printf("#%d\n", b1.ChainSlice[b].BlockNumber)
+		fmt.Printf("Nonce: %d\n", b1.ChainSlice[b].Nonce)
+		fmt.Printf("Body: %s\n", b1.ChainSlice[b].Body)
+		fmt.Printf("Prev: %x\n", b1.ChainSlice[b].PrevHashVal)
+		fmt.Printf("Hash: %x\n", b1.ChainSlice[b].HashVal)
 	}
 }
